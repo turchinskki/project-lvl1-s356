cae0f053acf56f92e3221bb047197ee8ad557386
Arthur <43511417+turchinskki@users.noreply.github.com>
2018-10-03 22:44:44 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import readlineSync from 'readline-sync';
  
-
 const getUserName = () => readlineSync.question('May I have your name? ');
 
 const getUserAnswer = () => readlineSync.question('Your answer: ');
