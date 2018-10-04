4561ea721b8844b4a1de46bfba01972b809c4eaf
Arthur <43511417+turchinskki@users.noreply.github.com>
2018-10-04 10:09:30 +0500

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import readlineSync from 'readline-sync';
- 
 
 const getUserName = () => readlineSync.question('May I have your name? ');
-
 const getUserAnswer = () => readlineSync.question('Your answer: ');
-
 const sayHelloToUser = () => {
   const name = getUserName();
   console.log(`Hello, ${name}!`);
